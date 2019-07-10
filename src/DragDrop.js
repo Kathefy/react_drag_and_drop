43efff0d9c2c9fe1f872425da507854efafb95ea
Katsiaryna Fedarovich <kathefy215@gmail.com>
2019-07-10 20:46:15 +0200

    Drop containers

--- src/DragDrop.js
+++ src/DragDrop.js
@@ -19,11 +19,54 @@ export default class DragDrop extends Component {
     };
     this.state.tasks.forEach(task => {
       tasks[task.category].push(
-        <div key={task.name} draggable className="draggable">
+        <div
+          key={task.name}
+          draggable
+          className="draggable"
+          onDragStart={e => this.onDragStart(e, task.name)}
+        >
           {task.name}
         </div>
       );
     });
-    return <div className="dragContainer">DRAG container</div>;
+    return (
+      <div className="dragContainer">
+        <h2 className="header">DRAG and DROP</h2>
+        <div
+          className="todo"
+          onDragOver={e => this.onDragOver(e)}
+          onDrop={e => {
+            this.onDrop(e, 'todo');
+          }}
+        >
+          <span className="task-header">TODO</span>
+          {tasks.todo}
+        </div>
+        <div
+          className="droppable"
+          onDragOver={e => this.onDragOver(e)}
+          onDrop={e => this.onDrop(e, 'doing')}
+        >
+          <span className="task-header">Doing</span>
+          {tasks.doing}
+        </div>
+        <div
+          className="droppable"
+          onDragOver={e => this.onDragOver(e)}
+          onDrop={e => this.onDrop(e, 'done')}
+        >
+          <span className="task-header">Done</span>
+          {tasks.done}
+        </div>
+        <div
+          className="droppable"
+          onDragOver={e => this.onDragOver(e)}
+          onDrop={e => this.onDrop(e, 'accepted')}
+        >
+          <span className="task-header">Accepted</span>
+          {tasks.accepted}
+        </div>
+      </div>
+    );
   }
 }
