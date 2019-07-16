dd669733ffba6bbd73e76fbad6ca5ce9a46f8ac1
Katsiaryna Fedarovich <kathefy215@gmail.com>
2019-07-16 23:29:57 +0200

    Drag and drop events

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import DragDrop from './DragDrop';
 import './App.css';
 
+const tasks = [
+  { name: 'Task 1', category: 'todo' },
+  { name: 'Task 2', category: 'doing' },
+  { name: 'Task 3', category: 'done' },
+  { name: 'Task 4', category: 'accepted' }
+];
+
 export default function App() {
-  return (
-    <div>
-      Drag And Drop
-      <DragDrop />
-    </div>
-  );
+  return <DragDrop tasks={tasks} />;
 }
--- src/DragDrop.js
+++ src/DragDrop.js
@@ -2,69 +2,123 @@ import React, { Component } from 'react';
 
 export default class DragDrop extends Component {
   state = {
-    tasks: [
-      { name: 'Task 1', category: 'todo' },
-      { name: 'Task 2', category: 'doing' },
-      { name: 'Task 3', category: 'done' },
-      { name: 'Task 4', category: 'accepted' }
-    ]
+    tasks: this.props.tasks
+  };
+
+  onDragStart = (e, id) => {
+    e.dataTransfer.setData('id', id);
+  };
+
+  onDragOver = e => {
+    e.preventDefault();
+  };
+
+  onDrop = (e, category) => {
+    let id = e.dataTransfer.getData('id');
+
+    let tasks = this.state.tasks.filter(task => {
+      if (task.name === id) {
+        task.category = category;
+      }
+      return task;
+    });
+
+    this.setState({
+      tasks
+    });
   };
 
   render() {
-    var tasks = {
-      todo: [],
-      doing: [],
-      done: [],
-      accepted: []
-    };
-    this.state.tasks.forEach(task => {
-      tasks[task.category].push(
-        <div
-          key={task.name}
-          draggable
-          className="draggable"
-          onDragStart={e => this.onDragStart(e, task.name)}
-        >
-          {task.name}
-        </div>
-      );
-    });
     return (
-      <div className="dragContainer">
+      <div className="container">
         <h2 className="header">DRAG and DROP</h2>
-        <div
-          className="todo"
-          onDragOver={e => this.onDragOver(e)}
-          onDrop={e => {
-            this.onDrop(e, 'todo');
-          }}
-        >
-          <span className="task-header">TODO</span>
-          {tasks.todo}
-        </div>
-        <div
-          className="droppable"
-          onDragOver={e => this.onDragOver(e)}
-          onDrop={e => this.onDrop(e, 'doing')}
-        >
-          <span className="task-header">Doing</span>
-          {tasks.doing}
-        </div>
-        <div
-          className="droppable"
-          onDragOver={e => this.onDragOver(e)}
-          onDrop={e => this.onDrop(e, 'done')}
-        >
-          <span className="task-header">Done</span>
-          {tasks.done}
-        </div>
-        <div
-          className="droppable"
-          onDragOver={e => this.onDragOver(e)}
-          onDrop={e => this.onDrop(e, 'accepted')}
-        >
-          <span className="task-header">Accepted</span>
-          {tasks.accepted}
+        <div className="dragContainer">
+          <div
+            className="droppable"
+            onDragOver={e => this.onDragOver(e)}
+            onDrop={e => {
+              this.onDrop(e, 'todo');
+            }}
+          >
+            <h4 className="taskHeader">ToDo</h4>
+            {this.state.tasks
+              .filter(task => task.category === 'todo')
+              .map(task => {
+                return (
+                  <div
+                    key={task.name}
+                    draggable
+                    className="draggable"
+                    onDragStart={e => this.onDragStart(e, task.name)}
+                  >
+                    {task.name}
+                  </div>
+                );
+              })}
+          </div>
+          <div
+            className="droppable"
+            onDragOver={e => this.onDragOver(e)}
+            onDrop={e => this.onDrop(e, 'doing')}
+          >
+            <h4 className="taskHeader">Doing</h4>
+            {this.state.tasks
+              .filter(task => task.category === 'doing')
+              .map(task => {
+                return (
+                  <div
+                    key={task.name}
+                    draggable
+                    className="draggable"
+                    onDragStart={e => this.onDragStart(e, task.name)}
+                  >
+                    {task.name}
+                  </div>
+                );
+              })}
+          </div>
+          <div
+            className="droppable"
+            onDragOver={e => this.onDragOver(e)}
+            onDrop={e => this.onDrop(e, 'done')}
+          >
+            <h4 className="taskHeader">Done</h4>
+            {this.state.tasks
+              .filter(task => task.category === 'done')
+              .map(task => {
+                return (
+                  <div
+                    key={task.name}
+                    draggable
+                    className="draggable"
+                    onDragStart={e => this.onDragStart(e, task.name)}
+                  >
+                    {task.name}
+                  </div>
+                );
+              })}
+          </div>
+          <div
+            className="droppable"
+            onDragOver={e => this.onDragOver(e)}
+            onDrop={e => this.onDrop(e, 'accepted')}
+          >
+            <h4 className="taskHeader">Accepted</h4>
+            {this.state.tasks
+              .filter(task => task.category === 'accepted')
+              .map(task => {
+                return (
+                  <div
+                    key={task.name}
+                    draggable
+                    className="draggable"
+                    onDragStart={e => this.onDragStart(e, task.name)}
+                  >
+                    {task.name}
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </div>
     );
